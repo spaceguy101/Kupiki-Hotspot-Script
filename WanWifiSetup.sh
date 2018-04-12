fdf2c3992ad1b49e2bba79387c01b8992fdcd296
Shreyas <shreyasponkshe12345@gmail.com>
2018-04-12 13:38:35 +0000

    Fir WpaConf

--- WanWifiSetup.sh
+++ WanWifiSetup.sh
@@ -8,8 +8,6 @@ EOT
 
 cat >> /etc/wpa_supplicant/wpa_supplicant.conf << EOT
 country=GB
-ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
-update_config=1
 network={
   ssid="Shreyas"
   psk="shreyasp101"
